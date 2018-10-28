f4b9cdf7f6d907c2797e081b6812193bb8769209
evendrow <evendrow>
2018-10-27 18:26:54 -0700

    Rockblock progress

--- ssi_project.ino
+++ ssi_project.ino
@@ -13,6 +13,11 @@
 #include <Adafruit_BNO055.h> //accelerometer
 #include <Adafruit_BMP280.h> //temp/pressure (BMP280)
 
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+
+#include <IridiumSBD.h>
+
 #define SAMPLERATE_DELAY_MS (1000)
 
 //--------------------------------------
@@ -22,8 +27,12 @@ typedef struct {
 } Thermo_t;
 
 typedef struct {
-  double latitude;
-  double longitude;
+  float sats;
+  float precision;
+  float latitude;
+  float longitude;
+  float alt;
+  float speed__kmph;
 } GPS_t;
 
 typedef struct {
@@ -53,7 +62,7 @@ Accel_t accel = {-1, -1, -1};
 // Create a thermocouple instance with software SPI 
 // on any three digital IO pins.
 #define MAXDO   7//12
-#define MAXCS   9//10
+#define MAXCS   21//9//10
 #define MAXCLK  14//13
 
 // Initialize the Thermocouple
@@ -79,6 +88,22 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 //--------------------------------------
 
 //--------------------------------------
+// Create GPS
+TinyGPS gpsChip;
+SoftwareSerial ss(0, 1);
+//--------------------------------------
+
+//--------------------------------------
+//Initialize RockBlock
+
+#define IridiumSerial Serial2
+#define DIAGNOSTICS true // Change this to see diagnostics
+
+// Declare the IridiumSBD object
+IridiumSBD modem(IridiumSerial);
+//--------------------------------------
+
+//--------------------------------------
 // SD Card setup
 
 // Set USE_SDIO to zero for SPI card access. 
@@ -94,9 +119,25 @@ const int8_t DISABLE_CHIP_SELECT = -1;
 SdFat sd;
 File myFile;
 
-const int chipSelect = 10;
+const int chipSelect = 20;//10;
 
 //--------------------------------------
+
+bool initializeSd();
+void writeLineToFile(String filename, String testLine);
+void readFileToConsole(String filename);
+
+void logDataToConsole();
+
+void checkSendRockblockMessage();
+void updateThermoTemp();
+void updateGPSData();
+void updateAccelData();
+void updateBMPData();
+void updateGPSData();
+void updateSensorData();
+
+static void smartdelay(unsigned long ms);
 
 void setup()
 {
@@ -105,16 +146,32 @@ void setup()
 //  SPI.setMISO(12); //DI pin; connected to DO on the sd reader.
 //  SPI.setSCK(13); //clock pin
 
+  //For RockBlock
+  int signalQuality = -1;
+  int err;
+
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    delay(1);
-  }
+  while (!Serial) { delay(1); }
 
-  //If SD fails to initialize, crash
-  if (!initializeSd()) {
+  // Start the serial port connected to the satellite modem
+  IridiumSerial.begin(19200);
+
+  // Begin satellite modem operation
+  Serial.println("Starting modem...");
+  err = modem.begin();
+  if (err != ISBD_SUCCESS)
+  {
+    Serial.print("Begin failed: error ");
+    Serial.println(err);
+    if (err == ISBD_NO_MODEM_DETECTED)
+      Serial.println("No modem detected: check wiring.");
     return;
   }
+  Serial.println("Success!");
+
+  //If SD fails to initialize, crash :(
+  if (!initializeSd()) { return; }
 
   //Try to initialize BMP
   if (!bmpChip.begin()) {  
@@ -128,14 +185,194 @@ void setup()
   }
   bno.setExtCrystalUse(true);
 
+  //GPS
+  ss.begin(9600);
   
-  
-
-  //Delay to wait for sensors to load
-  delay(1000);
+  //Delay to wait for sensors to load/stabilize
+  smartdelay(1000);
 
   //  writeLineToFile("test.txt", "This is a test line!");
   readFileToConsole("test.txt");
+}
+
+void loop() {
+
+  updateSensorData();
+  logDataToConsole();
+//  checkSendRockblockMessage();
+   
+  // Here we create a single line to input into the log file
+  String dataLog = "";
+  dataLog += millis();        dataLog += ","; // Timestamp
+  dataLog += thermo.temp;     dataLog += ","; // Thermocouple temperature
+  
+  dataLog += bmp.temp;        dataLog += ","; // BMP temperature
+  dataLog += bmp.pressure;    dataLog += ","; // BMP pressure
+  dataLog += bmp.alt;         dataLog += ","; // BMP altitude
+  
+  dataLog += accel.x;         dataLog += ","; // Accelerometer x
+  dataLog += accel.y;         dataLog += ","; // Accelerometer y
+  dataLog += accel.z;         dataLog += ","; // Accelerometer z
+
+  dataLog += gps.sats;        dataLog += ","; // GPS satellites
+  dataLog += gps.precision;   dataLog += ","; // GPS reading precision
+
+  // We use String(num, decimals) to force 5 decimal precision
+  // According the the internet, 5 decimals gives 1.1132 m precision (very good)
+  dataLog += String(gps.latitude, 5);    dataLog += ","; // GPS latitude
+  dataLog += String(gps.longitude, 5);   dataLog += ","; // GPS longitude
+  
+  dataLog += gps.alt;         dataLog += ","; // Accelerometer z
+  dataLog += gps.speed__kmph; //dataLog += ","; // Accelerometer z
+
+  Serial.println(dataLog);
+  Serial.println("");
+  
+  // Write the log to the log file
+  // The log file is in CSV format for easy data analysis
+//   writeLineToFile("log.csv", dataLog);
+  
+  // writeLineToFile("thermo.txt", thermoLog);
+
+  // Delay the next loop by a predetermined sample rate
+  // We use smartdelay() instead of delay() because we need to constantly
+  // update the GPS library with info from the serial ports
+  smartdelay(SAMPLERATE_DELAY_MS);
+
+  // OLD: Since data collection and logging take time, the
+  // OLD: actual sample rate will be higher
+}
+
+void logDataToConsole() {
+  //Log thermo data
+  String thermoLog = "[THERMO] C: ";
+  thermoLog += thermo.temp;
+  Serial.println(thermoLog);
+  
+  //Log bmp data
+  Serial.print("[BMP280] Temperature = ");
+  Serial.print(bmp.temp);
+  Serial.println(" *C");
+
+  Serial.print("[BMP280] Pressure = ");
+  Serial.print(bmp.pressure);
+  Serial.println(" Pa");
+
+  Serial.print("[BMP280] Approx altitude = ");
+  Serial.print(bmp.alt); // this should be adjusted to your local forcase
+  Serial.println(" m");
+
+  //Log accelerometer data
+  Serial.print("[ACCEL] ");
+  Serial.print("X: ");
+  Serial.print(accel.x, 4);
+  Serial.print("\tY: ");
+  Serial.print(accel.y, 4);
+  Serial.print("\tZ: ");
+  Serial.println(accel.z, 4);
+
+  //Log GPS data
+  Serial.print("[GPS] ");
+  Serial.print("Satellites: ");
+  Serial.print(gps.sats);
+  Serial.print("\t precision: ");
+  Serial.println(gps.precision);
+
+  Serial.print("[GPS] ");
+  Serial.print("latitude: ");
+  Serial.print(String(gps.latitude, 5));
+  Serial.print("\t longitude: ");
+  Serial.println(String(gps.longitude, 5));
+
+  Serial.print("[GPS] ");
+  Serial.print("altitude: ");
+  Serial.print(gps.alt);
+  Serial.print("\t speed: ");
+  Serial.println(gps.speed__kmph);
+
+  String message = "";
+  message += bmp.alt;         message += ","; // BMP altitude
+  message += bmp.temp;        message += ","; // BMP temperature
+
+  // We use String(num, decimals) to force 5 decimal precision
+  // According the the internet, 5 decimals gives 11.132 m precision (good)
+  message += String(gps.latitude, 5);    message += ","; // GPS latitude
+  message += String(gps.longitude, 5);   message += ","; // GPS longitude
+  
+  message += gps.alt;         message += ","; // Accelerometer z
+  message += bmp.pressure;    //message += ","; // BMP pressure
+
+  Serial.println(message);
+}
+
+void checkSendRockblockMessage() {
+  int signalQuality = -1;
+  
+  // This returns a number between 0 and 5.
+  // 2 or better is preferred.
+  int err = modem.getSignalQuality(signalQuality);
+  if (err != ISBD_SUCCESS)
+  {
+    Serial.print("SignalQuality failed: error ");
+    Serial.println(err);
+
+    writeLineToFile("log.csv", "Signal Quality Check failed");
+    return;
+  }
+
+  Serial.print("On a scale of 0 to 5, signal quality is currently ");
+  Serial.print(signalQuality);
+  Serial.println(".");
+
+
+  if (signalQuality > 1) {
+
+    //Altitude: altitude_barometer (m), internal_temperature, latitude, longitude, Altitude: altitude_gps (m), pressure
+    String message = "";
+    message += bmp.alt;         message += ","; // BMP altitude
+    message += bmp.temp;        message += ","; // BMP temperature
+
+    // We use String(num, decimals) to force 5 decimal precision
+    // According the the internet, 5 decimals gives 11.132 m precision (good)
+    message += String(gps.latitude, 5);    message += ","; // GPS latitude
+    message += String(gps.longitude, 5);   message += ","; // GPS longitude
+    
+    message += gps.alt;         message += ","; // Accelerometer z
+    message += bmp.pressure;    //message += ","; // BMP pressure
+
+    Serial.print(message);
+    
+    Serial.print("[ROCKBLOCK] Trying to send the message.  This might take several minutes.\r\n");
+    err = modem.sendSBDText("Hello world!");
+    if (err != ISBD_SUCCESS)
+    {
+      Serial.print("[ROCKBLOCK] sendSBDText failed: error ");
+      Serial.println(err);
+      if (err == ISBD_SENDRECEIVE_TIMEOUT)
+        Serial.println("[ROCKBLOCK] Try again with a better view of the sky.");
+    }
+  
+    else {
+      Serial.println("[ROCKBLOCK] Message send success");
+    }
+  } else {
+    Serial.println("[ROCKBLOCK] Signal too weak to send");
+  }
+}
+
+/*
+ * The gps library needs to constantly read from the serial for some reason
+ * So instead of delaying, we just keep feeding it data until the desired
+ * delay time has been reached
+ */
+static void smartdelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gpsChip.encode(ss.read());
+  } while (millis() - start < ms);
 }
 
 /*
@@ -233,6 +470,28 @@ void updateThermoTemp() {
 }
 
 /*
+ * Updates the GPS data structure with current data
+ * The GPS seems like it transmits data asynchronously
+ * Sets to -1 if any reading fails
+ */
+void updateGPSData() {
+
+  float flat, flon;
+  unsigned long age;
+  
+  gps.sats = ((gpsChip.satellites() == TinyGPS::GPS_INVALID_SATELLITES) ? -1 : gpsChip.satellites());
+  gps.precision = ((gpsChip.hdop() == TinyGPS::GPS_INVALID_HDOP) ? -1 : gpsChip.hdop());
+
+  gpsChip.f_get_position(&flat, &flon, &age);
+  gps.latitude = ((flat == TinyGPS::GPS_INVALID_F_ANGLE) ? -1 : flat);
+  gps.longitude = ((flon == TinyGPS::GPS_INVALID_F_ANGLE) ? -1 : flon);
+  
+  gps.alt = ((gpsChip.f_altitude() == TinyGPS::GPS_INVALID_F_ALTITUDE) ? -1 : gpsChip.f_altitude());
+  gps.speed__kmph = ((gpsChip.f_speed_kmph() == TinyGPS::GPS_INVALID_F_SPEED) ? -1 : gpsChip.f_speed_kmph());
+  
+}
+
+/*
  * Updates the accelerometer data structure with current data
  */
 void updateAccelData() {
@@ -244,6 +503,10 @@ void updateAccelData() {
   accel.z = event.orientation.z;
 }
 
+/*
+ * Update BMP data structure with current reading
+ * Make sure readAltitude() is populated with correct value
+ */
 void updateBMPData() {
   bmp.temp = bmpChip.readTemperature();
   bmp.pressure = bmpChip.readPressure();
@@ -257,59 +520,5 @@ void updateSensorData() {
   updateThermoTemp();
   updateAccelData();
   updateBMPData();
-}
-
-void loop() {
-
-  updateSensorData();
-  
-  //Log thermo data
-  String thermoLog = "[THERMO] C: ";
-  thermoLog += thermo.temp;
-  Serial.println(thermoLog);
-  
-  //Log bmp data
-  Serial.print("[BMP280] Temperature = ");
-  Serial.print(bmp.temp);
-  Serial.println(" *C");
-
-  Serial.print("[BMP280] Pressure = ");
-  Serial.print(bmp.pressure);
-  Serial.println(" Pa");
-
-  Serial.print("[BMP280] Approx altitude = ");
-  Serial.print(bmp.alt); // this should be adjusted to your local forcase
-  Serial.println(" m");
-
-  //Log accelerometer data
-  Serial.print("[ACCEL] ");
-  Serial.print("X: ");
-  Serial.print(accel.x, 4);
-  Serial.print("\tY: ");
-  Serial.print(accel.y, 4);
-  Serial.print("\tZ: ");
-  Serial.println(accel.z, 4);
-   
-  // Here we create a single line to input into the log file
-  String dataLog = "";
-  dataLog += millis();        dataLog += ","; // Timestamp
-  dataLog += thermo.temp;     dataLog += ","; // Thermocouple temperature
-  dataLog += bmp.temp;        dataLog += ","; // BMP temperature
-  dataLog += bmp.pressure;    dataLog += ","; // BMP pressure
-  dataLog += bmp.alt;         dataLog += ","; // BMP altitude
-  dataLog += accel.x;         dataLog += ","; // Accelerometer x
-  dataLog += accel.y;         dataLog += ","; // Accelerometer y
-  dataLog += accel.z;       //dataLog += ","; // Accelerometer z
-
-  Serial.println(dataLog);
-  Serial.println("");
-  
-  // Write the log to the log file
-  // The log file is in CSV format for easy data analysis
-//  writeLineToFile("thermo.txt", thermoLog);
-
-  // Delay the next loop by a predetermined sample rate
-  // Since data collection and logging take time, the
-  // actual sample rate will be higher
-   delay(SAMPLERATE_DELAY_MS);
+  updateGPSData();
 }
